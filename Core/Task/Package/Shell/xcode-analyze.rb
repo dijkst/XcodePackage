dc350f7254cb69cf4942e1f203a681a074851e71
Whirlwind <whirlwindjames@foxmail.com>
2016-09-30 19:44:06 +0800

    Fix analyze file reference which without name will lost.

--- Core/Task/Package/Shell/xcode-analyze.rb
+++ Core/Task/Package/Shell/xcode-analyze.rb
@@ -7,7 +7,7 @@ proj = Xcodeproj::Project.open(ARGV[0])
 json = proj.pretty_print
 
 frameworks = {}
-proj.files.select {|ref|ref.name =~ /\.(framework|a|tbd)$/i}.map {|ref| frameworks[ref.name || File.basename(ref.path)] = ref.path }
+proj.files.select {|ref|ref.path =~ /\.(framework|a|tbd)$/i}.map {|ref| frameworks[ref.name || File.basename(ref.path)] = ref.path }
 json["Frameworks"] = frameworks
 
 puts json.to_json
