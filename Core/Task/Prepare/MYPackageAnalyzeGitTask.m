991ed367dd86a89d929bbb87d5c040fb83589cec
Whirlwind <whirlwindjames@foxmail.com>
2016-11-11 10:48:41 +0800

    add support for ssh address
    Now it will get real url for ssh alias

--- Core/Task/Prepare/MYPackageAnalyzeGitTask.m
+++ Core/Task/Prepare/MYPackageAnalyzeGitTask.m
@@ -53,20 +53,31 @@
     // 主页
     if ([self executeCommand:@"git config remote.origin.url"] != 0) {
         self.errorMessage = @"获取 Git 服务器地址失败，请先设置服务端地址！";
+        return NO;
     } else {
-        self.config.gitUrl = self.shellTask.outputString;
+        self.config.gitUrl = [self.shellTask.outputString lowercaseString];
         NSString *homePage = self.config.gitUrl;
         if ([homePage hasPrefix:@"git@"]) {
             homePage = [homePage substringFromIndex:4];
         }
         if ([homePage hasSuffix:@".git"]) {
-            homePage = [homePage stringByDeletingPathExtension];
+            homePage = [homePage substringToIndex:[homePage length] - 4];
         }
         homePage = [homePage stringByReplacingOccurrencesOfString:@":(?=[^/])" withString:@"/" options:NSRegularExpressionSearch range:NSMakeRange(0, [homePage length])];
         if ([homePage rangeOfString:@"://"].location == NSNotFound) {
             homePage = [NSString stringWithFormat:@"http://%@", homePage];
         }
-        self.config.homePage = homePage;
+        NSURL *url = [NSURL URLWithString:homePage];
+        NSString *scheme = url.scheme;
+        if ([scheme isEqualToString:@"ssh"]) {
+            scheme = @"http";
+        }
+        if ([self executeCommand:[NSString stringWithFormat:@"ssh -G \"%@\" | awk '/^hostname / { print $2 }'", url.host]] != 0) {
+            self.errorMessage = @"解析 ssh Host 失败！";
+            return NO;
+        }
+        url = [[NSURL alloc] initWithScheme:scheme host:self.shellTask.outputString path:url.path];
+        self.config.homePage = [url absoluteString];
     }
     return YES;
 }
