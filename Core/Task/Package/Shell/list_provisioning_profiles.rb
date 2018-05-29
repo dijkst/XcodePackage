9aacf1c8d8b80ea1cb1f7a7518f69a0e500f7529
Whirlwind <whirlwindjames@foxmail.com>
2018-05-29 14:20:30 +0800

    remove debug log

--- Core/Task/Package/Shell/list_provisioning_profiles.rb
+++ Core/Task/Package/Shell/list_provisioning_profiles.rb
@@ -29,9 +29,5 @@ end
 
 if __FILE__ == $0
   require 'json'
-get_profiles.each do |team_id, array|
-    puts team_id
-    puts array.to_json
-    puts "---"
-    end
+  puts get_profiles.to_json
 end
