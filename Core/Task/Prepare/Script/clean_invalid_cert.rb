27bcdd0268ab65044c92a997f0523771f8aba033
Whirlwind <whirlwindjames@foxmail.com>
2018-06-02 19:25:34 +0800

    clean invalid cert and profile will work

--- Core/Task/Prepare/Script/clean_invalid_cert.rb
+++ Core/Task/Prepare/Script/clean_invalid_cert.rb
@@ -7,9 +7,14 @@ INVALID=[]
   match = line.match(/\s+\d+\) (\h+) "(.*)" \((.+)\)$/)
   next if match.nil?
   next if INVALID.include?(match[1])
-#  `/usr/bin/security delete-identity -Z #{match[1]} -t`
-  puts "#{match[1]} \"#{match[2]}\": #{match[3]}"
-  INVALID << match[1]
+  INVALID << match
 end
 
-puts "扫描无效代码签名证书完成！已清理 #{INVALID.length} 个。"
+puts "\n  Clean invalid identities:"
+maxlength = "#{INVALID.length}".length
+INVALID.each_with_index do |match, index|
+    space_count = maxlength - "#{index+1}".length + 1
+    puts "#{" " * space_count}#{index + 1}) #{match[1]} \"#{match[2]}\": #{match[3]}"
+    `/usr/bin/security delete-identity -Z #{match[1]} -t`
+end
+puts " #{" " * maxlength}  #{INVALID.length} invalid identities cleaned up"
--- Core/Task/Prepare/Script/clean_invalid_profile.rb
+++ Core/Task/Prepare/Script/clean_invalid_profile.rb
@@ -3,32 +3,39 @@
 
 require './list_provisioning_profiles.rb'
 
-count = 0
-def remove(profile, msg)
-  count += 1
-  puts "#{File.basename(profile.path)} \"#{profile.name}\": #{msg}"
-  File.delete(profile.path)
-  true
-end
+INVALID = []
+
+#COUNT = 0
+#def remove(profile, msg)
+#  COUNT += 1
+#  puts "#{File.basename(profile.path)} \"#{profile.name}\": #{msg}"
+##  File.delete(profile.path)
+#  true
+#end
 
 local_profiles = get_profiles
 
 local_profiles.each do |team_id, profiles|
   valid_bundle_id = []
-  profiles.delete_if do |profile|
+  profiles.each do |profile|
     if profile.certs.length == 0
-      remove(profile, "该 Profile 未发现任何有效证书被安装。")
+      INVALID << [profile, "未发现任何有效证书被安装"]
     elsif profile.expired?
-      remove(profile, "该 Profile 已过期。")
+      INVALID << [profile, "已过期"]
     elsif valid_bundle_id.include?(profile.bundle_id)
-      remove(profile, "该 Profile 过旧，存在更新的版本。")
+      INVALID << [profile, "存在更新的版本"]
     else
       valid_bundle_id << profile.bundle_id
-      false
     end
   end
 end
 
-puts "扫描无效描述文件完成！已清理 #{count} 个。"
+INVALID.group_by { |profile, msg| msg }.each do |msg, infos|
+    puts "#{msg}:"
+    infos.each do |profile, msg|
+        puts "  #{File.basename(profile.path)} #{profile.name}"
+        File.delete(profile.path)
+    end
+end
 
-local_profiles
+puts "扫描无效描述文件完成！已清理 #{INVALID.length} 个。"
